98ae8dd99f0cee247850433308d0647f3a6a32cc
Bshields87 <shields.bethany@gmail.com>
2019-04-16 14:35:13 -0700

    deployed

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ require("./routes/html-routes.js")(app);
 
 // Starts the server to begin listening
 // =============================================================
-db.sequelize.sync({force: true}
+db.sequelize.sync({force: false}
 ).then(function(){
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
